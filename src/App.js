8328f93a92868ecafb45741f81594f4d976ed096
pavantamidala <pavantamidala023@gmail.com>
2023-07-18 22:57:36 +0530

    v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const API_URL = 'https://emojihub.yurace.pro/api/all';
+
+const App = () => {
+  const [emojis, setEmojis] = useState([]);
+  const [filteredEmojis, setFilteredEmojis] = useState([]);
+  const [categoryFilter, setCategoryFilter] = useState('');
+  const [filteredCategories, setFilteredCategories] = useState([]);
+
+  useEffect(() => {
+    fetchEmojis();
+  }, []);
+
+  const fetchEmojis = async () => {
+    try {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      setEmojis(data);
+         // Extract unique categories
+      const uniqueCategories = new Set(data.map((emoji) => emoji.category));
+      setFilteredCategories(Array.from(uniqueCategories));
+      setFilteredEmojis(data);
+      setCategoryFilter('');
+      
+    } catch (error) {
+      console.error('Error fetching emojis:', error);
+    }
+  };
+
+  const handleCategoryFilterChange = (event) => {
+    const category = event.target.value;
+    setCategoryFilter(category);
+    filterEmojis(category);
+  };
+
+  const filterEmojis = (category) => {
+    if (category === '') {
+      setFilteredEmojis(emojis);
+    } else {
+      const filtered = emojis.filter((emoji) =>
+        emoji.category.toLowerCase().includes(category.toLowerCase())
+      );
+      setFilteredEmojis(filtered);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Emoji Hub</h1>
+      <div className="filter">
+        <label htmlFor="categoryFilter">Filter by category:</label>
+        <select
+          id="categoryFilter"
+          value={categoryFilter}
+          onChange={handleCategoryFilterChange}
         >
-          Learn React
-        </a>
-      </header>
+          <option value="">All</option>
+          {filteredCategories.map((category) => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="emoji-list">
+        {filteredEmojis.map((emoji,index) => (
+          <div key={index} className="emoji-card">
+            <div className="emoji">{emoji.htmlCode}</div>
+            <div className="details">
+              <p>Name: {emoji.name}</p>
+              <p>Category: {emoji.category}</p>
+              <p>Group: {emoji.group}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
